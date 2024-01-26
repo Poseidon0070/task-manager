858bf8f4bf796767db9d5486bc3cb7f1f7b67851
Shikhar <criticalxsynergy@gmail.com>
2024-01-26 22:42:18 +0530

    task-manager

--- src/components/TaskBox.js
+++ src/components/TaskBox.js
@@ -5,50 +5,50 @@ import { useDispatch } from 'react-redux';
 import { taskAction } from '../store/store';
 
 const customStyles = {
-  height: '175px',
-  width: '500px',
-  overflowX: 'auto',
+  height: '180px',
+  width: '400px',
+  overflowY: 'auto',
   padding: '5px',
   backgroundColor: '#f5fb1b2e',
   display: "flex",
-  flexDirection: 'row',
+  flexDirection: 'column',
 };
-
 
 function TaskBox({ title, date, description, _id }) {
   let dispatch = useDispatch()
   let removeTask = async (task_id) => {
-    try{
-        dispatch(taskAction.setLoading(true))
-        const res = await fetch(`http://localhost:8080/deleteTask/${task_id}`, {
-          method: 'DELETE',
-        })
-        dispatch(taskAction.setLoading(false))
-        if (res.ok) {
-          dispatch(taskAction.delete(task_id))
-        }
-    }catch(err){
+    try {
+      dispatch(taskAction.setLoading(true))
+      const res = await fetch(`http://localhost:8080/deleteTask/${task_id}`, {
+        method: 'DELETE',
+      })
+      dispatch(taskAction.setLoading(false))
+      if (res.ok) {
+        dispatch(taskAction.delete(task_id))
+      }
+    } catch (err) {
       console.log(err)
-    }finally{
+    } finally {
       dispatch(taskAction.setLoading(false))
     }
   }
 
   return (
     <Paper elevation={2} className='task-box' sx={{ ...customStyles, justifyContent: "space-between", borderLeft: '5px solid orange' }}>
-      <div className='p-1'>
-        <h2 className='mt-2 mx-3'>{title}</h2>
+      <div className='p-0'>
+        <h4 className='mt-0 mx-3'>{title === '' ? 'No title' : title}</h4>
         <hr className='m-2' />
-        <p className='mx-3 my-1'>created at : {date}</p>
-        <p className='mx-3'>{description}</p>
+        <p className='mx-3 font-monospace'>{description === '' ? 'No description' : description}</p>
       </div>
-      <div className='d-flex justify-content-end align-self-end'>
+      <div className='d-flex justify-content-between'>
+        <p className='mx-3 mb-2 fw-medium'>created at : {date === '' ? 'Not mentioned' : date}</p>
         <DeleteIcon fontSize='large'
           sx={{
-            transitionProperty : "cursor, transform",
+            transitionProperty: "cursor, transform",
+            justifySelf : 'flex-end',
             transition: '200ms ease-in-out',
             '&:hover': {
-              cursor : "pointer",
+              cursor: "pointer",
               transform: 'scale(1.09)',
             },
           }}
